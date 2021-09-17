391dbbcfc4397339db5de2ae61d58a76269058f4
wallanmota <wallanmota.13@gmail.com>
2021-09-17 17:32:30 -0300

    logica e impressao de valores adicionados

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let numAdicionado = [];
+
 function add(){
     let numero = document.querySelector('#numero').value;
     let numAd = document.querySelector('#numAd');
@@ -9,20 +11,36 @@ function add(){
         item.text = `Valor ${numero} adicionado`;
         numAd.appendChild(item);
         numAdicionado.push(numero)
-        console.log(numAdicionado)
     }
 }
-let numAdicionado = [];
-numAdicionado.sort();
-let quantItem = numAdicionado.length;
 
 function enviar(){
-    let quantidade = document.querySelector('#quantidade')
-    let maior = document.querySelector('#maior')
-    let menor = document.querySelector('#menor')
-    let soma = document.querySelector('#soma')
-    let media = document.querySelector('#media')
+    var quantItem = numAdicionado.length;
+    numAdicionado.sort((a,b)=>{return a-b});
+    
+    if(quantItem == 0){
+        alert(`Adicione um valor antes de finalizar`)
+    }
+    else{
+        let quantidade = document.querySelector('#quantidade')
+        let maior = document.querySelector('#maior')
+        let menor = document.querySelector('#menor')
+        let soma = document.querySelector('#soma')
+        let media = document.querySelector('#media')
 
-    quantidade.innerHTML = `Ao todo, temos ${numAdicionado.length} números cadastrados.`;
-    maior.innerHTML = `O maior valor informado foi ${numAdicionado[Number(quantItem)]}`;
+        quantidade.innerHTML = `Ao todo, temos ${quantItem} números cadastrados.`;
+        maior.innerHTML = `O maior valor informado foi ${numAdicionado[quantItem -1]}`;
+        menor.innerHTML = `O menor valor informado foi ${numAdicionado[0]}`;
+        soma.innerHTML = `Somando todos os valores, temos ${sum()}`;
+        media.innerHTML = `A média dos valores digitados é ${sum()/quantItem}`;
+    }
 }
+
+function sum(){
+    let soma = 0
+    for(let i = 0; i < numAdicionado.length; i++){
+        soma += Number(numAdicionado[i])
+    }return soma
+}
+
+
